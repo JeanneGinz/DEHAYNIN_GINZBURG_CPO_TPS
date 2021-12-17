2b78cb441ce35c2917fcd93886aba20e83360d13
jeann <jeann@LAPTOP-DV54OCDC>
2021-12-17 12:49:51 +0100

    création classe joueur

---
+++ Mini jeu/src/Cellules.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jeann
+ */
+public class Cellules {
+     Cellules casejoue; //Création d'un jeton relié à la classe jeton 
+
+    public Cellules() {
+        casejoue = null; // Le jeton présent ou non sur une case de la grille
+    }
+
+    
+}
