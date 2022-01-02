6dc1ea91acc6487d6a60242f88dfd381f5fdbbc3
Jade <Jade@macbook-pro-de-jade.home>
2022-01-03 00:07:29 +0100

    constructeur grille

--- Mini jeu/src/mini/jeu/Grille.java
+++ Mini jeu/src/mini/jeu/Grille.java
@@ -9,5 +9,15 @@ package mini.jeu;
  * @author Jade
  */
 public class Grille {
+    Bouton[][]GrillePartie = new Bouton[6][6];
     
+    public Grille(){ //constructeur qui initialise notre grille
+        for (int i=0;i<6;i++){ // on reprend celle de notre puissance 4 
+            for (int j=0;j<6;j++){
+                GrillePartie[i][j]=new Bouton();
+                GrillePartie[i][j].boutonAlume=false; // avec en plus on initialise nos bouton en mode eteint
+            }
+            
+        }
+    }
 }
