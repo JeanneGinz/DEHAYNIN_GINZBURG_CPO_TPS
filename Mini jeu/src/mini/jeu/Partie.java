edf30a6c16e684297c753f99e60515486320c8e9
Jade <Jade@macbook-pro-de-jade.home>
2022-01-03 11:20:12 +0100

    creation methode partie

--- Mini jeu/src/mini/jeu/Partie.java
+++ Mini jeu/src/mini/jeu/Partie.java
@@ -4,10 +4,30 @@
  */
 package mini.jeu;
 
+import java.util.Scanner;
+import java.util.Timer;
+
 /**
  *
  * @author Jade
  */
 public class Partie {
+    String Joueur1;
+    Grille GrillePartie;
+    Timer chrono = new Timer();
+    int tour=0;
+    
+    public Partie(){
+        tour=0;
+    }
+    public void InitialiserPartie(){
+        Grille GrilleBouton = new Grille();
+    }
+    
+    public void DebuterPartie(){
+        Scanner sca = new Scanner(System.in);
+        System.out.println("Entrez le nom du joueur ");
+    }
+    
     
 }
