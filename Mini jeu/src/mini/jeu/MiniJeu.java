15fe9de0827ff98c8f184dd9eae2f4e0b4d3893d
jeann <jeann@LAPTOP-DV54OCDC>
2022-01-03 02:05:04 +0100

    boutton cliquable

--- Mini jeu/src/mini/jeu/MiniJeu.java
+++ Mini jeu/src/mini/jeu/MiniJeu.java
@@ -5,8 +5,11 @@
 package mini.jeu;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -19,20 +22,57 @@ public class MiniJeu { // Speed clic
     /**
      * @param args the command line arguments
      */
+    
+    
+    
+    public static ImageIcon DimensionImage(String cheminImage, int largeur , int hauteur ){
+        ImageIcon srcIcon = new ImageIcon(cheminImage); // on cherche  l'image enregistré 
+        Image srcImage= srcIcon.getImage(); // extraction des données d'image 2D
+        Image resizedImage= srcImage.getScaledInstance(largeur, hauteur, Image.SCALE_SMOOTH);
+        
+        ImageIcon resizedIcon=new ImageIcon(resizedImage); // régénère l'image aux dimensionx voulu
+        
+        return resizedIcon;
+    }
     public static void main(String[] args) {
            JFrame frame1 = new JFrame("image coronavirus");
         frame1.setPreferredSize(new Dimension(800,600));
-        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         
        ImageIcon coronavirus = new ImageIcon("./images/coronavirus.png");
-        JLabel jlabel1 =new JLabel(coronavirus);
+       ImageIcon NVcoronavirus = DimensionImage("./images/coronavirus.png", 100, 50);
+        JLabel jlabel1 =new JLabel(NVcoronavirus);
         jlabel1.setBorder(BorderFactory.createTitledBorder("coronavirus"));
-       // frame1.add(jlabel1);
+        frame1.add(jlabel1);
         
         frame1.pack();
         frame1.setVisible(true);
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // pour faire bouton cliquable 
+           JFrame frame2 = new JFrame("image coronavirus");
+        frame2.setPreferredSize(new Dimension(800,600));
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame2.setLayout(new FlowLayout());
+        
+        JButton button1=new JButton ("Clique");
+        button1.setPreferredSize(new Dimension(200,100));
+        frame2.add(button1);
+      // ImageIcon coronavirus = new ImageIcon("./images/coronavirus.png");
+       //ImageIcon NVcoronavirus = DimensionImage("./images/coronavirus.png", 100, 50);
+        JLabel jlabel2 =new JLabel(NVcoronavirus);
+        jlabel1.setBorder(BorderFactory.createTitledBorder("coronavirus"));
+        frame1.add(jlabel2);
+        
+        frame2.pack();
+        frame2.setVisible(true);
+        
+        
+        
+        
         
     }
     }
-    
 
+   
+     
