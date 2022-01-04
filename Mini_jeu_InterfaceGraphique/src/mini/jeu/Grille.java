a51076c37ef1a2ca59e906e2f46afa65cde44a3d
jeann <jeann@LAPTOP-DV54OCDC>
2022-01-04 14:59:30 +0100

    FENETRE interface graphique

---
+++ Mini_jeu_InterfaceGraphique/src/mini/jeu/Grille.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mini.jeu;
+import java.util.Random;
+/**
+ *
+ * @author Jade
+ */
+public class Grille {
+    Bouton[][]GrillePartie = new Bouton[6][6];
+    
+    public Grille(){ //constructeur qui initialise notre grille
+        for (int i=0;i<6;i++){ // on reprend celle de notre puissance 4 
+            for (int j=0;j<6;j++){
+                GrillePartie[i][j]=new Bouton();
+                GrillePartie[i][j].boutonAllume=false; // avec en plus on initialise nos bouton en mode eteint
+            }
+            
+        }
+    }
+    
+    public void BoutonAleatoirementAllume(){
+        Random random = new Random();
+        int nbColonne;
+        int nbLigne;
+        nbColonne = random.nextInt(5);
+        nbLigne=random.nextInt(5);
+        GrillePartie[nbColonne][nbLigne].AllumerLeBouton();
+        GrillePartie[nbColonne][nbLigne].couleurDuBouton("coronavirus");
+        
+        int piege;
+        piege=random.nextInt(2);
+        if (piege == 1) {
+        int x = random.nextInt(6);
+        int y = random.nextInt(6);
+        while (GrillePartie[nbColonne][nbLigne] == GrillePartie[x][y]) { // on vérifie que notre masque ne soit pas sur notre covid
+              x = random.nextInt(6); 
+              y = random.nextInt(6);
+            }
+            GrillePartie[x][y].AllumerLeBouton();
+            GrillePartie[x][y].couleurDuBouton("masque");
+        
+    }
+
+    int booster;
+    booster=random.nextInt(5);
+    if(booster==3){
+        int x = random.nextInt(6);
+        int y = random.nextInt(6);
+        while (GrillePartie[nbColonne][nbLigne] == GrillePartie[x][y]) { // on vérifie que notre masque ne soit pas sur notre covid
+              x = random.nextInt(6); 
+              y = random.nextInt(6);
+            }
+            GrillePartie[x][y].AllumerLeBouton();
+            GrillePartie[x][y].couleurDuBouton("delta");
+        
+    }
+    }
+    public boolean verificationBoutonClique(int colonne, int ligne) {
+        return GrillePartie[colonne][ligne].boutonAllume;
+    }
+
+    public void CliquerPourEteindre(int colonne, int ligne) {
+         if (GrillePartie[colonne][ligne].boutonAllume == true ) { 
+        GrillePartie[colonne][ligne].EteindreLeBouton(); 
+        }
+    } 
+    
+    void AfficherGrille(){ // On reprend la meme methode que notre puissance 4 avec des petites modifs
+        for (int i=0; i<6; i++) {
+            for (int j=0; j<6; j++) {
+                if(GrillePartie[i][j].boutonAllume == false){
+                    System.out.print(" E "); // pour eteint
+                }
+                else if (GrillePartie[i][j].boutonAllume == true) {
+                    System.out.print(" A "); //pour allumé
+        }
+    }
+             System.out.println(); 
+        }
+    }
+   // public int 
+}
+
+    
+
---
+++ Mini_jeu_InterfaceGraphique/src/mini/jeu/Partie.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mini.jeu;
+
+import java.util.Scanner;
+import java.util.Timer;
+import java.io.*;
+import java.lang.Thread;
+
+/**
+ *
+ * @author Jade
+ */
+public class Partie {
+    String Joueur1;
+    Grille GrilleJeu;
+    Timer chrono = new Timer();
+    int tour=0;
+    
+    public Partie(){
+        tour=0;
+    }
+    public void InitialiserPartie(){
+         GrilleJeu = new Grille();
+    }
+    
+    public void DebuterPartie() throws InterruptedException{
+        Scanner sca = new Scanner(System.in);
+        System.out.println("Entrez le nom du joueur ");
+        Joueur1 = sca.next();
+        
+        InitialiserPartie();
+       
+        GrilleJeu.AfficherGrille();
+        System.out.println("La partie va débuter");
+        //long t= System.currentTimeMillis();
+        //long end = t+30000;
+        while(tour < 4) { 
+            GrilleJeu.BoutonAleatoirementAllume();
+            GrilleJeu.AfficherGrille();
+            Scanner sc = new Scanner(System.in); 
+            System.out.println("saisissez la ligne entre 1 et 6");
+            int a = sc.nextInt() -1;
+            System.out.println("saisissez la colonne entre 1 et 6");
+            int b = sc.nextInt() -1;
+            GrilleJeu.CliquerPourEteindre(a,b);
+            tour++;
+        
+  
+                //Thread.sleep(30000);
+        
+    }
+    
+    
+}
+}
